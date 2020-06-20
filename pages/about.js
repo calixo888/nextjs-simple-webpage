b33f9bb28eddde4a482c18abd08eb4ce77185d99
Calix Huang <calix.huang1@gmail.com>
2020-06-19 22:27:19 -0700

    change

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout.js'
 export default function About() {
   return (
     <Layout>
-      <h1>Hello!</h1>
+      <h1>Hello there!</h1>
       <p>This will be pink</p>
 
       <style jsx>{`
